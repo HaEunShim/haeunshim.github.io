2b29582d7f6815cc29e4f02523bbe500e4fd6521
HaEunShim <underthesea517@gmail.com>
2025-10-11 14:43:22 +0000

    Deploying to gh-pages from @ HaEunShim/haeunshim.github.io@04d320593101ed1b3fcca6fba5fed8e4da3b5cd0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,16 +71,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-will-be-presenting-an-oral-talk-at-the-2025-interspeech-rotterdam-netherlands",
+          title: 'I will be presenting an oral talk at the 2025 INTERSPEECH, Rotterdam, Netherlands....',
           description: "",
           section: "News",},{
         id: 'social-email',
